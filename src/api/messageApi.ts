1c868987f8f7b5e4f56b9b93262d65e2961dbb30
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-10-22 20:16:25 +0530

    log correct

--- src/api/messageApi.ts
+++ src/api/messageApi.ts
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 import { PaginatedMessagesResponse } from "@/type/message";
 
 export const fetchMessagesByRoom = async (roomId: string, page = 1, size = 10) => {
-  console.log('fetch message',roomId,page,size)
+  console.info('fetch message for room-id',roomId,'page:',page,'size:' ,size)
   const response = await axiosInstance.get<PaginatedMessagesResponse>(
     `/api/room/${roomId}/message?offset=${page}&size=${size}&sortDir=desc&sortBy=sentAt`
   );
